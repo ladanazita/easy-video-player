31a0798f4ea9b7da3dd61d7ef4cf6e8213d1bde4
ladan nasserian <ladannasserian@gmail.com>
2017-08-10 11:02:51 -0700

    Implements Segment-Nielsen SDK

--- sample/src/main/java/com/afollestad/easyvideoplayersample/MainActivity.java
+++ sample/src/main/java/com/afollestad/easyvideoplayersample/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.Toast;
 import com.afollestad.easyvideoplayer.EasyVideoCallback;
 import com.afollestad.easyvideoplayer.EasyVideoPlayer;
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.segment.analytics.Analytics;
+import com.segment.analytics.Properties;
+import com.segment.analytics.android.integrations.nielsendcr.NielsenDCRIntegration;
 
 public class MainActivity extends AppCompatActivity implements EasyVideoCallback {
 
@@ -22,19 +25,71 @@ public class MainActivity extends AppCompatActivity implements EasyVideoCallback
     assert player != null;
     player.setCallback(this);
     // All further configuration is done from the XML layout.
+    Analytics analytics = new Analytics.Builder(this, "QDjpO9jNyjJGAMnH55VlEpPgbOvSAcP9")
+        .use(NielsenDCRIntegration.FACTORY)
+        .flushQueueSize(1)
+        .trackApplicationLifecycleEvents()
+        .recordScreenViews()
+        .build();
+
+    // Set the initialized instance as a globally accessible instance.
+    Analytics.setSingletonInstance(analytics);
   }
 
   @Override
   protected void onPause() {
+    Analytics.with(this).track("Video Playback Paused", new Properties()
+        .putValue("assetId", 1234)
+        .putValue("adType", "mid-roll")
+        .putValue("totalLength", 100)
+        .putValue("videoPlayer", "vimeo")
+        .putValue("playbackPosition", 10)
+        .putValue("fullScreen", true)
+        .putValue("bitrate", 50)
+        .putValue("sound", 80));
     super.onPause();
     player.pause();
   }
 
   @Override
-  public void onStarted(EasyVideoPlayer player) {}
+  public void onStarted(EasyVideoPlayer player) {
+    Analytics.with(this).track("Video Playback Started", new Properties()
+        .putValue("assetId", 1234)
+        .putValue("adType", "pre-roll")
+        .putValue("totalLength", 120)
+        .putValue("videoPlayer", "youtube")
+        .putValue("sound", 80)
+        .putValue("bitrate", 40)
+        .putValue("fullScreen", true)
+    );
+
+    Analytics.with(this).track("Video Content Started", new Properties()
+        .putValue("assetId", 123214)
+        .putValue("title", "Look Who's Purging Now")
+        .putValue("season", 2)
+        .putValue("episode", 9)
+        .putValue("genre", "cartoon")
+        .putValue("program", "Rick and Morty")
+        .putValue("channel", "cartoon network")
+        .putValue("publisher", "Turner Broadcasting System")
+        .putValue("fullEpisode", true)
+        .putValue("podId", "segment A")
+        .putValue("playbackPosition", 70)
+    );
+  }
 
   @Override
-  public void onPaused(EasyVideoPlayer player) {}
+  public void onPaused(EasyVideoPlayer player) {
+    Analytics.with(this).track("Video Playback Paused", new Properties()
+        .putValue("assetId", 1234)
+        .putValue("adType", "mid-roll")
+        .putValue("totalLength", 100)
+        .putValue("videoPlayer", "vimeo")
+        .putValue("playbackPosition", 10)
+        .putValue("fullScreen", true)
+        .putValue("bitrate", 50)
+        .putValue("sound", 80));
+  }
 
   @Override
   public void onPreparing(EasyVideoPlayer player) {
@@ -48,6 +103,15 @@ public class MainActivity extends AppCompatActivity implements EasyVideoCallback
 
   @Override
   public void onBuffering(int percent) {
+      Analytics.with(this).track("Video Playback Buffer Started", new Properties()
+              .putValue("assetId", 7890)
+              .putValue("adType", "post-roll")
+              .putValue("totalLength", 700)
+              .putValue("videoPlayer", "youtube")
+              .putValue("playbackPosition", 20)
+              .putValue("fullScreen", false)
+              .putValue("bitrate", 500)
+              .putValue("sound", 80));
     Log.d("EVP-Sample", "onBuffering(): " + percent + "%");
   }
 
@@ -63,6 +127,15 @@ public class MainActivity extends AppCompatActivity implements EasyVideoCallback
 
   @Override
   public void onCompletion(EasyVideoPlayer player) {
+    Analytics.with(this).track("Video Content Completed", new Properties()
+        .putValue("assetId", 7890)
+        .putValue("adType", "post-roll")
+        .putValue("totalLength", 700)
+        .putValue("videoPlayer", "youtube")
+        .putValue("playbackPosition", 20)
+        .putValue("fullScreen", false)
+        .putValue("bitrate", 500)
+        .putValue("sound", 80));
     Log.d("EVP-Sample", "onCompletion()");
   }
 
